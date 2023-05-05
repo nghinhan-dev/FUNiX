394a5487e173a3f83d8a765a7f3900b4e1d5d068
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2023-05-05 15:37:26 +0700

    try optimize

--- react/Assignment_02/src/pages/browse/MovieList/MovieList.jsx
+++ react/Assignment_02/src/pages/browse/MovieList/MovieList.jsx
@@ -19,11 +19,14 @@ export default function MovieList({ data }) {
   });
 
   useEffect(() => {
-    let active = true;
+    const controller = new AbortController();
+    const signal = controller.signal;
+
     const fetchVideo = async () => {
       try {
         const respond = await fetch(
-          `https://api.themoviedb.org/3/movie/${showPortal?.id}/videos?api_key=a41e04665d0188e4b15ad25b23931766`
+          `https://api.themoviedb.org/3/movie/${showPortal?.id}/videos?api_key=a41e04665d0188e4b15ad25b23931766`,
+          { signal }
         );
 
         if (!respond.ok) {
@@ -36,12 +39,10 @@ export default function MovieList({ data }) {
 
         const vidData = await respond.json();
 
-        if (active) {
-          setPortalData((prevState) => ({
-            ...prevState,
-            video: vidData.results,
-          }));
-        }
+        setPortalData((prevState) => ({
+          ...prevState,
+          video: vidData.results,
+        }));
       } catch (error) {
         console.log(error.message);
       }
@@ -50,77 +51,38 @@ export default function MovieList({ data }) {
     fetchVideo();
 
     return () => {
-      active = false;
+      controller.abort();
     };
   }, [showPortal.id]);
 
-  function renderPortalVideo() {
-    let arrayVideo = portalData.video;
-    if (arrayVideo == []) {
-      return (
-        <img
-          className="portal__img"
-          src={`https://image.tmdb.org/t/p/w500${portalData.data.backdrop_path}`}
-          alt={portalData.data.name + "jpg"}
-          draggable={false}
-        />
-      );
-    }
+  return (
+    <>
+      {data && renderCategoryList(data)}
 
-    let findTrailerIndex = arrayVideo.findIndex(
-      (vid) => vid.type === "Trailer"
-    );
+      {showPortal.isShow &&
+        createPortal(
+          <div id="portal">
+            <div className="portal__container">
+              <div className="movie_desc">
+                <h3>
+                  {portalData.data?.original_name ||
+                    portalData.data?.original_title}
+                </h3>
 
-    if (findTrailerIndex != -1) {
-      return (
-        <iframe
-          width="100%"
-          height="400"
-          src={`https://www.youtube.com/embed/${arrayVideo[findTrailerIndex].key}`}
-        ></iframe>
-      );
-    } else {
-      let findTeaserIndex = arrayVideo.findIndex(
-        (vid) => vid.type === "Teaser"
-      );
-
-      if (findTeaserIndex != -1) {
-        return (
-          <iframe
-            width="100%"
-            height="400"
-            src={`https://www.youtube.com/embed/${arrayVideo[findTrailerIndex].key}`}
-          ></iframe>
-        );
-      } else {
-        return (
-          <img
-            className="portal__img"
-            src={`https://image.tmdb.org/t/p/w500${portalData.data.backdrop_path}`}
-            alt={portalData.data.name + "jpg"}
-            draggable={false}
-          />
-        );
-      }
-    }
-  }
-
-  function displayPortal(movie, obj) {
-    if (movie.id == obj.id && obj.isShow) {
-      // click on the same image (same id) && portal is already show
-      setShowPortal((prevState) => ({ ...prevState, isShow: false }));
-    } else {
-      setShowPortal((prevState) => ({
-        ...prevState,
-        id: movie.id,
-        isShow: true,
-      }));
-      setPortalData((prevState) => ({
-        ...prevState,
-        data: movie,
-      }));
-    }
-  }
+                <hr />
+                <p>Release Date: {portalData.data.release_date}</p>
+                <p>Vote: {portalData.data.vote_average}/10</p>
+                <p className="overview">{portalData.data.overview}</p>
+              </div>
+              <div className="movie_mv">
+                {portalData.video && renderPortalVideo()}
+              </div>
+            </div>
+          </div>,
+          document.getElementById(portalData.destinate)
+        )}
+    </>
+  );
 
   function renderMovieList(list, isPoster) {
     let content = list.map((movie) => {
@@ -197,31 +159,72 @@ export default function MovieList({ data }) {
     });
   }
 
-  return (
-    <>
-      {data && renderCategoryList(data)}
+  function displayPortal(movie, obj) {
+    if (movie.id == obj.id && obj.isShow) {
+      // click on the same image (same id) && portal is already show
+      setShowPortal((prevState) => ({ ...prevState, isShow: false }));
+    } else {
+      setShowPortal((prevState) => ({
+        ...prevState,
+        id: movie.id,
+        isShow: true,
+      }));
+      setPortalData((prevState) => ({
+        ...prevState,
+        data: movie,
+      }));
+    }
+  }
 
-      {showPortal.isShow &&
-        createPortal(
-          <div id="portal">
-            <div className="portal__container">
-              <div className="movie_desc">
-                <h3>
-                  {portalData.data?.original_name ||
-                    portalData.data?.original_title}
-                </h3>
-                <hr />
-                <p>Release Date: {portalData.data.release_date}</p>
-                <p>Vote: {portalData.data.vote_average}/10</p>
-                <p className="overview">{portalData.data.overview}</p>
-              </div>
-              <div className="movie_mv">
-                {portalData.video && renderPortalVideo()}
-              </div>
-            </div>
-          </div>,
-          document.getElementById(portalData.destinate)
-        )}
-    </>
-  );
+  function renderPortalVideo() {
+    let arrayVideo = portalData.video;
+
+    if (arrayVideo == []) {
+      return (
+        <img
+          className="portal__img"
+          src={`https://image.tmdb.org/t/p/w500${portalData.data.backdrop_path}`}
+          alt={portalData.data.name + "jpg"}
+          draggable={false}
+        />
+      );
+    }
+
+    let findTrailerIndex = arrayVideo.findIndex(
+      (vid) => vid.type === "Trailer" && vid.site === "Youtube"
+    );
+
+    if (findTrailerIndex !== -1) {
+      return (
+        <iframe
+          width="100%"
+          height="400"
+          src={`https://www.youtube.com/embed/${arrayVideo[findTrailerIndex].key}`}
+        ></iframe>
+      );
+    } else {
+      let findTeaserIndex = arrayVideo.findIndex(
+        (vid) => vid.type === "Teaser"
+      );
+
+      if (findTeaserIndex !== -1) {
+        return (
+          <iframe
+            width="100%"
+            height="400"
+            src={`https://www.youtube.com/embed/${arrayVideo[findTeaserIndex].key}`}
+          ></iframe>
+        );
+      } else {
+        return (
+          <img
+            className="portal__img"
+            src={`https://image.tmdb.org/t/p/w500${portalData.data.backdrop_path}`}
+            alt={portalData.data.name + "jpg"}
+            draggable={false}
+          />
+        );
+      }
+    }
+  }
 }
