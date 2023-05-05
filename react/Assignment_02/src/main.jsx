8503b49cce6e7f15856e6857f490fef6f43d51f6
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2023-05-05 15:37:39 +0700

    add router.jsx

--- react/Assignment_02/src/main.jsx
+++ react/Assignment_02/src/main.jsx
@@ -1,28 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Browse from "./pages/browse/Browse";
-import Search from "./pages/search/Search";
-import Navbar from "./navbar/Navbar";
-import ErrorPage from "./pages/error/error-page";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Navbar />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "",
-        element: <Browse />,
-      },
-      {
-        path: "/search",
-        element: <Search />,
-      },
-    ],
-  },
-]);
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
