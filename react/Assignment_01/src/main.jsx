10a8510e94720ea4a96faa1182d641e38bef9672
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2023-05-05 14:31:36 +0700

    update Router path

--- react/Assignment_01/src/main.jsx
+++ react/Assignment_01/src/main.jsx
@@ -4,21 +4,27 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Search from "./pages/search/Search";
 import Detail from "./pages/detail/Detail";
+import Layout from "./Layout/Layout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "/Detail",
+        element: <Detail />,
+      },
+    ],
   },
 
   {
     path: "/Search",
     element: <Search />,
-  },
-
-  {
-    path: "/Detail",
-    element: <Detail />,
   },
 ]);
 
