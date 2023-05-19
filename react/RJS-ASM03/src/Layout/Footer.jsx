665f349293416151258a7351fc029d5ad9dd4bae
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2023-05-19 07:12:36 +0700

    complete Layout

--- react/RJS-ASM03/src/Layout/Footer.jsx
+++ react/RJS-ASM03/src/Layout/Footer.jsx
@@ -1,3 +1,35 @@
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div id="footer" className="bg-dark">
+      <Container>
+        <Row className="py-3 text-light">
+          <Col className="d-flex flex-column">
+            <p className="fw-semibold">CUSTOMER SERVICES</p>
+            <a href="#">Help & Contact Us</a>
+            <a href="#">Returns & Refunds</a>
+            <a href="#">Online Stores</a>
+            <a href="#">Terms & Conditions</a>
+          </Col>
+          <Col className="d-flex flex-column">
+            <p className="fw-semibold">COMPANY</p>
+            <a href="#">What We Do</a>
+            <a href="#">Availabel Services</a>
+            <a href="#">Latest Posts</a>
+            <a href="#">FAQs</a>
+          </Col>
+          <Col className="d-flex flex-column">
+            <p className="fw-semibold">SOCIAL MEDIA</p>
+            <a href="#">Twitter</a>
+            <a href="#">Instagram</a>
+            <a href="#">Facebook</a>
+            <a href="#">Printerest</a>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
--- react/RJS-ASM03/src/Layout/MyNavbar.jsx
+++ react/RJS-ASM03/src/Layout/MyNavbar.jsx
@@ -1,42 +1,82 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { LinkContainer } from "react-router-bootstrap";
+import { NavLink } from "react-router-dom";
 
 export default function MyNavbar() {
+  // !!! NavLink from "react-router-dom" NOT from"react-bootstrap"
   return (
     <Navbar className="fw-bold" sticky="top" bg="light" expand="lg">
       <Container>
-        <Row className="py-2 g-0 align-items-center w-100">
+        <Row className="g-0 align-items-center w-100">
           <Col>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-              <LinkContainer to="/">
-                <Nav.Link className="pe-5">Home</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/shop">
-                <Nav.Link>Shop</Nav.Link>
-              </LinkContainer>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? "active pe-5 nav-link" : "pe-5 nav-link"
+                }
+                style={({ isActive }) => {
+                  return {
+                    color: isActive ? "#dfb44f" : "#000",
+                  };
+                }}
+              >
+                Home
+              </NavLink>
+              <NavLink
+                to="/shop"
+                className={({ isActive }) =>
+                  isActive ? "active nav-link" : "nav-link"
+                }
+                style={({ isActive }) => {
+                  return {
+                    color: isActive ? "#dfb44f" : "#000",
+                  };
+                }}
+              >
+                Shop
+              </NavLink>
             </Navbar.Collapse>
           </Col>
           <Col className="d-flex justify-content-center">
             <Navbar.Brand className="fs-4">BOTIQUE</Navbar.Brand>
           </Col>
           <Col className="flex-row justify-content-end navbar-nav">
-            <LinkContainer to="/cart">
-              <Nav.Link className="d-flex align-items-center flex-row pe-5">
+            <NavLink
+              to="/cart"
+              className={({ isActive }) =>
+                isActive ? "active nav-link pe-5" : "nav-link pe-5"
+              }
+              style={({ isActive }) => {
+                return {
+                  color: isActive ? "#dfb44f" : "#000",
+                };
+              }}
+            >
+              <div className="d-flex align-items-center flex-row pe-5">
                 <i className="fa-solid fa-cart-shopping pe-1"></i>
                 <p className="d-none d-md-block">Cart</p>
-              </Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/login">
-              <Nav.Link className="d-flex  align-items-center flex-row ">
+              </div>
+            </NavLink>
+            <NavLink
+              to="/login"
+              className={({ isActive }) =>
+                isActive ? "active pe-5 nav-link" : "pe-5 nav-link"
+              }
+              style={({ isActive }) => {
+                return {
+                  color: isActive ? "#dfb44f" : "#000",
+                };
+              }}
+            >
+              <div className="d-flex  align-items-center flex-row ">
                 <i className="fa-solid fa-user pe-1"></i>
                 <p className="d-none d-md-block">Login</p>
-              </Nav.Link>
-            </LinkContainer>
+              </div>
+            </NavLink>
           </Col>
         </Row>
       </Container>
