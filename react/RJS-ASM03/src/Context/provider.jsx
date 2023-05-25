69f88104574977596799cd04dd66776f5ade3410
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2023-05-25 11:10:20 +0700

    update user's cart, load user's cart on init

--- react/RJS-ASM03/src/Context/provider.jsx
+++ react/RJS-ASM03/src/Context/provider.jsx
@@ -2,17 +2,47 @@
 import { useEffect, useState } from "react";
 import { CurrentUserContext } from "./context";
 import { UserArr } from "./context";
+import { useDispatch, useSelector } from "react-redux";
+import { cartAction } from "../store/store";
 
 export default function UserProvider({ children }) {
+  const dispatch = useDispatch();
+  const currentIndex = useSelector((state) => state.currentUserIndex.index);
+  const cart = useSelector((state) => state.cart);
+
   const [currentUser, setCurrentUser] = useState(null);
   const [userArr, setUserArr] = useState(
     JSON.parse(localStorage.getItem("userArr")) || []
   );
 
   useEffect(() => {
-    const getCurrentUser = JSON.parse(localStorage.getItem("CURRENT_USER"));
-    setCurrentUser(getCurrentUser);
+    const currentUser = JSON.parse(localStorage.getItem("CURRENT_USER"));
+
+    if (currentUser !== null) {
+      dispatch(cartAction.UPDATE_CART(currentUser.cart));
+    }
+    setCurrentUser(currentUser);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  useEffect(() => {
+    if (currentUser) {
+      let updateCurrentUserArr = currentUser;
+      updateCurrentUserArr.cart = cart;
+      setCurrentUser(updateCurrentUserArr);
+      localStorage.setItem(
+        "CURRENT_USER",
+        JSON.stringify(updateCurrentUserArr)
+      );
+
+      let updateUserArr = userArr;
+      updateUserArr[currentIndex] = currentUser;
+      setUserArr(updateUserArr);
+      localStorage.setItem("userArr", JSON.stringify(updateUserArr));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cart]);
 
   useEffect(() => {
     // Save the userArr to localStorage
