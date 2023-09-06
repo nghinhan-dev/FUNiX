12306fd1b65ad96c5491fabadf1bb52a9c695daa
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2023-09-06 10:12:30 +0700

    add validation

--- nodejs/Assignment 02/admin/src/Users/AddUser.jsx
+++ nodejs/Assignment 02/admin/src/Users/AddUser.jsx
@@ -1,7 +1,15 @@
-import { useState } from "react";
-import { Form } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Form, useActionData } from "react-router-dom";
+import { toastError } from "../util/toast";
+import { ToastContainer } from "react-toastify";
 
 export default function AddUser() {
+  const errors = useActionData();
+
+  useEffect(() => {
+    errors?.confirm && toastError(errors.unmatch);
+  }, [errors?.confirm, errors?.unmatch]);
+
   const [formInput, setFormInput] = useState({
     username: "",
     password: "",
@@ -21,173 +29,187 @@ export default function AddUser() {
   };
 
   return (
-    <section id="render_data">
-      <Form method="POST">
-        <div className="header">
-          <h3>Add User</h3>
-          <button className="btn btn-add" type="submit">
-            Submit
-          </button>
-        </div>
-        <div className="add-form-container">
-          <div className="table_container add-display main-shadow">
-            <code>
-              <p className="bracket">{`{`}</p>
-              <p>
-                username: <span>sillywhale</span>,
-              </p>
-              <p>
-                password: <span>123456</span>,
-              </p>
-              <p>
-                fullName: <span>Nguyen Nghi Nhan</span>,
-              </p>
-              <p>
-                phoneNumber: <span>763218899</span>,
-              </p>
-              <p>
-                email: <span>sillywhale@sw.com</span>,
-              </p>
-              <p>
-                isAdmin: <span>true</span>,
-              </p>
-              <p className="bracket">{`}`}</p>
-            </code>
+    <>
+      <section id="render_data">
+        <Form method="POST">
+          <div className="header">
+            <h3>Add User</h3>
+            <button className="btn btn-add" type="submit">
+              Submit
+            </button>
           </div>
-          <div className="form-inputs">
-            <div>
-              <label
-                style={
-                  formInput.username.length !== 0
-                    ? labelHasTextStyle
-                    : labelEmptyStyle
-                }
-              >
-                Username
-              </label>
-              <input
-                type="text"
-                name="username"
-                onChange={(e) =>
-                  setFormInput((prevState) => ({
-                    ...prevState,
-                    username: e.target.value,
-                  }))
-                }
-              />
+          <div className="add-form-container">
+            <div className="table_container add-display main-shadow">
+              <code>
+                <p className="bracket">{`{`}</p>
+                <p>
+                  username: <span>{formInput.username}</span>,
+                </p>
+                <p>
+                  password: <span>{formInput.password}</span>,
+                </p>
+                <p>
+                  fullName: <span>{formInput.fullname}</span>,
+                </p>
+                <p>
+                  phoneNumber: <span>{formInput.phone}</span>,
+                </p>
+                <p>
+                  email: <span>{formInput.email}</span>,
+                </p>
+                <p>
+                  isAdmin:
+                  <span>{formInput.isAdmin ? "true" : "false"}</span>,
+                </p>
+                <p className="bracket">{`}`}</p>
+              </code>
             </div>
-            <div>
-              <label
-                style={
-                  formInput.password.length !== 0
-                    ? labelHasTextStyle
-                    : labelEmptyStyle
-                }
-              >
-                Password
-              </label>
-              <input
-                type="text"
-                name="password"
-                onChange={(e) =>
-                  setFormInput((prevState) => ({
-                    ...prevState,
-                    password: e.target.value,
-                  }))
-                }
-              />
-            </div>
-            <div>
-              <label
-                style={
-                  formInput.confirm.length !== 0
-                    ? labelHasTextStyle
-                    : labelEmptyStyle
-                }
-              >
-                Confirm password
-              </label>
-              <input
-                type="text"
-                name="confirm"
-                onChange={(e) =>
-                  setFormInput((prevState) => ({
-                    ...prevState,
-                    confirm: e.target.value,
-                  }))
-                }
-              />
-            </div>
-            <div>
-              <label
-                style={
-                  formInput.fullname.length !== 0
-                    ? labelHasTextStyle
-                    : labelEmptyStyle
-                }
-              >
-                Full Name
-              </label>
-              <input
-                type="text"
-                name="fullname"
-                onChange={(e) =>
-                  setFormInput((prevState) => ({
-                    ...prevState,
-                    fullname: e.target.value,
-                  }))
-                }
-              />
-            </div>
-            <div>
-              <label
-                style={
-                  formInput.phone.length !== 0
-                    ? labelHasTextStyle
-                    : labelEmptyStyle
-                }
-              >
-                Phone
-              </label>
-              <input
-                type="text"
-                name="phone"
-                onChange={(e) =>
-                  setFormInput((prevState) => ({
-                    ...prevState,
-                    phone: e.target.value,
-                  }))
-                }
-              />
-            </div>
-            <div>
-              <label
-                style={
-                  formInput.email.length !== 0
-                    ? labelHasTextStyle
-                    : labelEmptyStyle
-                }
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                name="email"
-                onChange={(e) =>
-                  setFormInput((prevState) => ({
-                    ...prevState,
-                    email: e.target.value,
-                  }))
-                }
-              />
-            </div>
-            <div>
-              isAdmin
-              <input type="checkbox" name="isAdmin" />
+            <div className="form-inputs">
+              <div>
+                <label
+                  style={
+                    formInput.username.length !== 0
+                      ? labelHasTextStyle
+                      : labelEmptyStyle
+                  }
+                >
+                  Username
+                </label>
+                <input
+                  type="text"
+                  name="username"
+                  onChange={(e) =>
+                    setFormInput((prevState) => ({
+                      ...prevState,
+                      username: e.target.value,
+                    }))
+                  }
+                />
+              </div>
+              <div>
+                <label
+                  style={
+                    formInput.password.length !== 0
+                      ? labelHasTextStyle
+                      : labelEmptyStyle
+                  }
+                >
+                  Password
+                </label>
+                <input
+                  type="text"
+                  name="password"
+                  onChange={(e) =>
+                    setFormInput((prevState) => ({
+                      ...prevState,
+                      password: e.target.value,
+                    }))
+                  }
+                />
+              </div>
+              <div>
+                <label
+                  style={
+                    formInput.confirm.length !== 0
+                      ? labelHasTextStyle
+                      : labelEmptyStyle
+                  }
+                >
+                  Confirm password
+                </label>
+                <input
+                  type="text"
+                  name="confirm"
+                  onChange={(e) =>
+                    setFormInput((prevState) => ({
+                      ...prevState,
+                      confirm: e.target.value,
+                    }))
+                  }
+                />
+              </div>
+              <div>
+                <label
+                  style={
+                    formInput.fullname.length !== 0
+                      ? labelHasTextStyle
+                      : labelEmptyStyle
+                  }
+                >
+                  Full Name
+                </label>
+                <input
+                  type="text"
+                  name="fullname"
+                  onChange={(e) =>
+                    setFormInput((prevState) => ({
+                      ...prevState,
+                      fullname: e.target.value,
+                    }))
+                  }
+                />
+              </div>
+              <div>
+                <label
+                  style={
+                    formInput.phone.length !== 0
+                      ? labelHasTextStyle
+                      : labelEmptyStyle
+                  }
+                >
+                  Phone
+                </label>
+                <input
+                  type="text"
+                  name="phone"
+                  onChange={(e) =>
+                    setFormInput((prevState) => ({
+                      ...prevState,
+                      phone: e.target.value,
+                    }))
+                  }
+                />
+              </div>
+              <div>
+                <label
+                  style={
+                    formInput.email.length !== 0
+                      ? labelHasTextStyle
+                      : labelEmptyStyle
+                  }
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  onChange={(e) =>
+                    setFormInput((prevState) => ({
+                      ...prevState,
+                      email: e.target.value,
+                    }))
+                  }
+                />
+              </div>
+              <div>
+                isAdmin
+                <input
+                  type="checkbox"
+                  name="isAdmin"
+                  onChange={(e) => {
+                    console.log(e.target.value === "on");
+                    return setFormInput((prevState) => ({
+                      ...prevState,
+                      isAdmin: e.target.checked,
+                    }));
+                  }}
+                />
+              </div>
             </div>
           </div>
-        </div>
-      </Form>
-    </section>
+        </Form>
+      </section>
+      <ToastContainer limit={1} />
+    </>
   );
 }
---
+++ nodejs/Assignment 02/admin/src/util/toast.js
@@ -0,0 +1,14 @@
+import { toast } from "react-toastify";
+
+export const toastError = (text) => {
+  return toast.error(text, {
+    position: "top-center",
+    autoClose: 2000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  });
+};
