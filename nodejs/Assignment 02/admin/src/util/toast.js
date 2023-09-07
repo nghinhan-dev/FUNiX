df74d5df176e5939b0ab65d5063367ddeed6c6cd
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2023-09-07 13:39:17 +0700

    add toastSuccess to util/toast

--- nodejs/Assignment 02/admin/src/util/toast.js
+++ nodejs/Assignment 02/admin/src/util/toast.js
@@ -12,3 +12,16 @@ export const toastError = (text) => {
     theme: "light",
   });
 };
+
+export const toastSuccess = (text) => {
+  return toast.success(text, {
+    position: "top-center",
+    autoClose: 2000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  });
+};
