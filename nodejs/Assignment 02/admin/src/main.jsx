4d5d6767c713f9497dc15632034937e11a85ed9b
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2023-09-12 14:57:20 +0700

    add react-daterange-picker package

--- nodejs/Assignment 02/admin/src/main.jsx
+++ nodejs/Assignment 02/admin/src/main.jsx
@@ -5,6 +5,8 @@ import router from "./router.jsx";
 
 import "./css/style.css";
 import "react-toastify/dist/ReactToastify.css";
+import "@wojtekmaj/react-daterange-picker/dist/DateRangePicker.css";
+import "react-calendar/dist/Calendar.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
