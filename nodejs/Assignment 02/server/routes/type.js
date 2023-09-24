3a729c3b1cce0914b12af65dea0e70121ae7eb35
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2023-09-24 14:40:51 +0700

    add PUT method

--- nodejs/Assignment 02/server/routes/type.js
+++ nodejs/Assignment 02/server/routes/type.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const typeController = require("../controllers/typeController");
+// validator
+const validate = require("../middleware/validate");
 
 // TypeRooms methods
 router.get("/get_type_rooms", typeController.getTypeRoom);
 router.get("/type/:id", typeController.getSpecificType);
+router.put("/type/:id", validate.addType, typeController.updateRoom);
 
 module.exports = router;
