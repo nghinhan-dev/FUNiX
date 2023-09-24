dc0a710834abeeb46e378db50f44bc770f19ce1a
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2023-09-24 14:39:12 +0700

    add EditType method

--- nodejs/Assignment 02/server/controllers/typeController.js
+++ nodejs/Assignment 02/server/controllers/typeController.js
@@ -1,4 +1,5 @@
 const TypeofRoom = require("../model/TypesofRoom");
+const Room = require("../model/Room");
 
 exports.getSpecificType = async (req, res) => {
   const id = req.params.id;
@@ -26,4 +27,39 @@ exports.getTypeRoom = async (req, res) => {
     console.log("error:", error);
     res.status(409).send(`${error}`);
   }
+};
+
+exports.updateRoom = async (req, res, next) => {
+  const typeId = req.params.id;
+  const updateData = {
+    price: req.body.price,
+    roomNums: req.body.roomNums,
+    desc: req.body.desc,
+    title: req.body.title,
+    maxPeople: req.body.maxPeople,
+  };
+
+  const roomIds = await Promise.all(
+    updateData.roomNums.split(",").map(async (numRoom) => {
+      const newType = new Room({
+        number: numRoom * 1,
+        bookedRange: [],
+      });
+      await newType.save();
+
+      return newType._id;
+    })
+  );
+
+  updateData.roomNums = roomIds;
+
+  const updatedType = await TypeofRoom.findByIdAndUpdate(typeId, updateData, {
+    new: true,
+  });
+
+  if (!updatedType) {
+    return res.status(404).send({ statusText: "Type room not found" });
+  }
+
+  res.status(200).send({ statusText: `Updated ${typeId}` });
 };
