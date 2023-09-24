88eaacd6344edd530b2c60d6b6249c6e3800ac13
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2023-09-24 14:39:31 +0700

    restructure & add validateArrNum

--- nodejs/Assignment 02/server/middleware/validate.js
+++ nodejs/Assignment 02/server/middleware/validate.js
@@ -51,14 +51,7 @@ exports.addRoom = async (req, res, next) => {
 
 exports.addHotel = async (req, res, next) => {
   const data = req.body;
-  const validationErrors = [];
-
-  for (const [key, value] of Object.entries(data)) {
-    const error = isEmpty(key, value);
-    if (error) {
-      validationErrors.push(error);
-    }
-  }
+  const validationErrors = validator(data);
 
   if (validationErrors.length > 0) {
     res.status(400).json({ errors: validationErrors });
@@ -67,30 +60,92 @@ exports.addHotel = async (req, res, next) => {
   }
 };
 
-function isNumber(key, value, res) {
-  if (typeof value !== "number") {
-    return res.status(400).send({ statusText: `${key} must be a number` });
+exports.addType = async (req, res, next) => {
+  const data = req.body;
+  const validationErrors = validator(data);
+
+  if (validationErrors.length > 0) {
+    res.status(400).json({ errors: validationErrors });
+  } else {
+    next(); // No errors, proceed to the next middleware
   }
+};
 
-  if (value === 0) {
-    return res.status(400).send({ statusText: `Missing ${key} in body` });
-  }
+function validator(data) {
+  const validationErrors = [];
 
-  return;
-}
-
-function isEmpty(key, value) {
-  if (value.length === 0) {
-    return `${key} cannot be empty`;
-  }
-
-  // Validate array of strings
-  if (key === "type" || key === "rooms" || key === "photos") {
-    const arrValue = value.split(",");
-    if (arrValue.some((currentValue) => currentValue.trim() === "")) {
-      return `Contain empty value in ${key}`;
+  for (const [key, value] of Object.entries(data)) {
+    const errors = validateProperty(key, value);
+    if (errors) {
+      validationErrors.push(...errors);
     }
   }
 
+  return validationErrors;
+}
+
+function validateProperty(key, value) {
+  switch (key) {
+    case "title":
+    case "name":
+    case "city":
+    case "address":
+    case "desc":
+      return validateNotEmpty(key, value);
+
+    case "cheapestPrice":
+    case "rating":
+    case "number":
+    case "price":
+    case "maxPeople":
+    case "phoneNumber":
+      return validateNumber(key, value);
+
+    case "type":
+    case "rooms":
+    case "photos":
+      return validateArrayNotEmpty(key, value);
+
+    case "roomNums":
+      return validateArrayIsNumber(key, value);
+
+    default:
+      return null; // No error
+  }
+}
+
+function validateNotEmpty(key, value) {
+  if (!value || value.trim() === "") {
+    return [`${key} cannot be empty`];
+  }
+  return null; // No error
+}
+
+function validateNumber(key, value) {
+  if (isNaN(value)) {
+    return [`${key} must be a number`];
+  }
+  if (value === 0) {
+    return [`Missing ${key} in body`];
+  }
+  return null; // No error
+}
+
+function validateArrayNotEmpty(key, value) {
+  const arrValue = value.split(",");
+  if (arrValue.some((currentValue) => currentValue.trim() === "")) {
+    return [`Contain empty value in ${key}`];
+  }
+  return null; // No error
+}
+
+function validateArrayIsNumber(key, value) {
+  const arrValue = value.split(",");
+  if (arrValue.some((currentValue) => isNaN(currentValue))) {
+    return [`Contain Not-a-Number value in ${key}`];
+  }
+  if (arrValue.some((currentValue) => currentValue === 0)) {
+    return [`Contain empty value in ${key}`];
+  }
   return null; // No error
 }
