f8256304de9026c1b2f29b3d044d187e501a6ddc
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2023-09-26 10:31:38 +0700

    add main ToastContainer

--- nodejs/Assignment 02/admin/src/MainLayout/MainLayout.jsx
+++ nodejs/Assignment 02/admin/src/MainLayout/MainLayout.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar/Navbar";
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 export default function MainLayout() {
   return (
@@ -9,6 +10,7 @@ export default function MainLayout() {
         <Navbar />
       </div>
       <Outlet />
+      <ToastContainer />
     </>
   );
 }
