6fd393ffa0e5198ee4775cd367fdf5643b23a612
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2023-10-02 12:22:14 +0700

    add default

--- nodejs/Assignment 02/server/model/TypesofRoom.js
+++ nodejs/Assignment 02/server/model/TypesofRoom.js
@@ -3,9 +3,18 @@ const { Schema } = mongoose;
 
 const typesofRoomSchema = new Schema({
   title: String,
-  price: Number,
-  maxPeople: Number,
-  desc: String,
+  price: {
+    type: Number,
+    default: 0,
+  },
+  maxPeople: {
+    type: Number,
+    default: 0,
+  },
+  desc: {
+    type: String,
+    default: "",
+  },
   roomIds: [String],
 });
 
