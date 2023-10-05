a8d30659e1f523c26fe28a6d542fba16da97e4c3
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2023-10-05 13:07:37 +0700

    convert date form api to string

--- nodejs/Assignment 02/admin/src/util/timeZone.js
+++ nodejs/Assignment 02/admin/src/util/timeZone.js
@@ -8,3 +8,8 @@ const dateStringOpt = {
 export const formattedDateInTimeZone = (originalDate) => {
   return originalDate.toLocaleDateString(undefined, dateStringOpt);
 };
+
+export const formatDateFormAPI = (dateString) => {
+  const newDate = new Date(dateString);
+  return formattedDateInTimeZone(newDate);
+};
