81789e51e8a0a9550af0d33e2ac0db1d76ed3ab9
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2023-11-01 11:03:54 +0700

    update new func name

--- nodejs/Assignment 02/server/routes/type.js
+++ nodejs/Assignment 02/server/routes/type.js
@@ -6,9 +6,9 @@ const typeController = require("../controllers/typeController");
 const validate = require("../middleware/validate");
 
 // TypeRooms methods
-router.post("/add_type", validate.validType, typeController.addType);
+router.post("/add_type", validate.validateReqBody, typeController.addType);
 router.get("/get_type_rooms", typeController.getTypeRoom);
 router.get("/type/:id", typeController.getSpecificType);
-router.put("/type/:id", validate.validType, typeController.updateType);
+router.put("/type/:id", validate.validateReqBody, typeController.updateType);
 
 module.exports = router;
