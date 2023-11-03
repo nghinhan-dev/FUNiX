9ffe76dca886cc86de03d536b0fd0bbe72c1195e
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2023-11-03 09:01:08 +0700

    Create Transaction route

--- nodejs/Assignment 02/client/src/pages/transaction/Transactions.jsx
+++ nodejs/Assignment 02/client/src/pages/transaction/Transactions.jsx
@@ -1,8 +1,45 @@
 import { useLoaderData } from "react-router-dom";
+import { formatDate } from "../../util/formatDate";
 
 export default function Transactions() {
   const data = useLoaderData();
-  console.log("data:", data);
 
-  return <section>Transactions</section>;
+  const renderTransactions = data.map((trans, index) => {
+    return (
+      <tr key={trans._id}>
+        <td>{`0${index + 1}`}</td>
+        <td>{trans.hotel}</td>
+        <td>{trans.roomNums.join(",")}</td>
+        <td>
+          {formatDate(trans.dateStart)} - {formatDate(trans.dateEnd)}
+        </td>
+        <td>${trans.total}</td>
+        <td>{trans.payment}</td>
+        <td>
+          <span className={`status status-${trans.status.toLowerCase()}`}>
+            {trans.status}
+          </span>
+        </td>
+      </tr>
+    );
+  });
+
+  return (
+    <section className="transaction__container">
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Hotel</th>
+            <th>Room</th>
+            <th>Date</th>
+            <th>Price</th>
+            <th>Payment Method</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>{renderTransactions}</tbody>
+      </table>
+    </section>
+  );
 }
