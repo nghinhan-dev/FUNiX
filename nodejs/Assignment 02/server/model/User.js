6929a52aa8d40266e6c6ade7f6f255fe3b7fb329
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2023-11-03 08:55:37 +0700

    Change phoneNumber data type number -> string

--- nodejs/Assignment 02/server/model/User.js
+++ nodejs/Assignment 02/server/model/User.js
@@ -5,7 +5,7 @@ const userSchema = new Schema({
   username: String,
   password: String,
   fullName: String,
-  phoneNumber: Number,
+  phoneNumber: String,
   email: String,
   isAdmin: Boolean,
 });
