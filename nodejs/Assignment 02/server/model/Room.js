aecb75318876a08b002b8ab6d3f600997db756e7
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2023-11-03 08:54:55 +0700

    Remove _id in bookedRangeSchema

--- nodejs/Assignment 02/server/model/Room.js
+++ nodejs/Assignment 02/server/model/Room.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const bookedRangeSchema = new Schema({
-  startDate: Date,
-  endDate: Date,
-});
+const bookedRangeSchema = new Schema(
+  {
+    startDate: Date,
+    endDate: Date,
+  },
+  { _id: false }
+);
 
 const roomSchema = new Schema({
   number: { type: Number, default: 0 }, // Adding default value 0
