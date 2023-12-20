04d99427d86d8a9b35b3651628b3488c96a77c41
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2023-12-20 17:20:51 +0700

    -u refractor to mongoose

--- nodejs/Lab 12-14/backend/model/user.js
+++ nodejs/Lab 12-14/backend/model/user.js
@@ -1,121 +1,51 @@
-const { getDB } = require("../util/database");
+const mongoose = require("mongoose");
 const { ObjectId } = require("mongodb");
-class User {
-  constructor(userName, email, id, cart) {
-    this.userName = userName;
-    this.email = email;
-    this._id = id ? new ObjectId(id) : null;
-    this.cart = cart;
+
+const cartItemSchema = new mongoose.Schema(
+  {
+    id: String,
+    quantity: {
+      default: 1,
+      type: Number,
+    },
+  },
+  {
+    _id: false,
   }
+);
 
-  async save() {
-    const db = getDB();
+const userSchema = new mongoose.Schema({
+  userName: String,
+  password: String,
+  email: String,
+  cart: {
+    items: [cartItemSchema],
+  },
+});
 
-    return await db.collection("users").insertOne(this);
-  }
+// METHODS
+userSchema.methods.addToCart = async function addToCart(product) {
+  const cart = [...this.cart.items];
+  const itemIndex = cart.findIndex((item) => {
+    return item.id === product._id.toString();
+  });
 
-  async addToCart(product) {
-    const db = getDB();
-    let newQuantity = 1;
-    const updatedCartItems = [...this.cart.items];
-
-    const cartItemIndex = this.cart.items.findIndex(
-      (book) => book.prdID.toString() === product._id.toString()
-    );
-
-    if (cartItemIndex !== -1) {
-      newQuantity = this.cart.items[cartItemIndex].quantity + 1;
-      updatedCartItems[cartItemIndex].quantity = newQuantity;
-    } else {
-      updatedCartItems.push({
-        prdID: new ObjectId(product._id),
-        quantity: newQuantity,
-      });
-    }
-
-    const updatedCart = { items: updatedCartItems };
-
-    return db
-      .collection("users")
-      .updateOne(
-        { _id: new ObjectId(this._id) },
-        { $set: { cart: updatedCart } }
-      );
-  }
-
-  async getCart() {
-    const db = getDB();
-    const productIds = this.cart.items.map((prd) => prd.prdID);
-    const products = await db
-      .collection("books")
-      .find({ _id: { $in: productIds } })
-      .toArray();
-
-    const cartItems = products.map((book) => {
-      return {
-        ...book,
-        quantity: this.cart.items.find(
-          (item) => item.prdID.toString() === book._id.toString()
-        ).quantity,
-      };
+  if (itemIndex !== -1) {
+    cart[itemIndex].quantity++;
+  } else {
+    cart.push({
+      id: new ObjectId(product._id),
     });
-
-    return cartItems;
   }
 
-  async delFromCart(delId) {
-    const db = getDB();
+  this.cart.items = cart;
+  await this.save();
 
-    const updatedCartItems = this.cart.items.filter(
-      (book) => book.prdID.toString() !== delId.toString()
-    );
+  // You can also return the updated cart if needed
+  return cart;
+};
 
-    return await db
-      .collection("users")
-      .updateOne(
-        { _id: new ObjectId(this._id) },
-        { $set: { cart: { items: updatedCartItems } } }
-      );
-  }
+userSchema.methods.getOrder = async function getOrder() {};
 
-  async addOrder() {
-    const db = getDB();
-    try {
-      const products = await this.getCart();
-      const order = {
-        items: products,
-        user: {
-          _id: new ObjectId(this._id),
-          userName: this.userName,
-        },
-      };
-
-      await db.collection("orders").insertOne(order);
-      await db
-        .collection("users")
-        .updateOne(
-          { _id: new ObjectId(this._id) },
-          { $set: { cart: { items: [] } } }
-        );
-    } catch (error) {
-      console.log("error:", error);
-    }
-  }
-
-  async getOrder() {
-    const db = getDB();
-
-    return db
-      .collection("orders")
-      .find({ "user._id": new ObjectId(this._id) })
-      .toArray();
-  }
-
-  static async findById(userId) {
-    const db = getDB();
-
-    return await db.collection("users").findOne({ _id: new ObjectId(userId) });
-  }
-}
-
+const User = mongoose.model("User", userSchema);
 module.exports = User;
