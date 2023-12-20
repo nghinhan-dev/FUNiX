4d7f147215bd4ce0620f1f0c000261313312364d
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2023-12-20 17:25:17 +0700

    -a cart routes

--- nodejs/Lab 12-14/backend/routes/shop.js
+++ nodejs/Lab 12-14/backend/routes/shop.js
@@ -5,10 +5,12 @@ const booksController = require("../controllers/books");
 const shopController = require("../controllers/cart");
 const orderController = require("../controllers/order");
 
+const loginAuth = require("../middleware/auth");
+
 router.get("/book_list", booksController.getBookList);
-router.get("/cart", shopController.getCart);
+router.get("/cart", loginAuth.loginAuth, shopController.getCart);
 router.post("/edit-book", booksController.updateBook);
-router.post("/add-cart", shopController.postToCart);
+router.post("/add-cart", loginAuth.loginAuth, shopController.postToCart);
 router.post("/delete-item", shopController.delFromCart);
 router.post("/create-order", orderController.postOrder);
 router.get("/get-orders", orderController.getOrder);
