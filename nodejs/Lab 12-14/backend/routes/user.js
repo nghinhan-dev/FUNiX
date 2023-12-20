f2bd4dc0f1e56928acf81b79726aacf963c2c659
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2023-12-20 17:25:29 +0700

    -a sign up route

--- nodejs/Lab 12-14/backend/routes/user.js
+++ nodejs/Lab 12-14/backend/routes/user.js
@@ -5,5 +5,6 @@ const userController = require("../controllers/user");
 
 router.post("/login", userController.login);
 router.post("/logout", userController.logout);
+router.post("/signup", userController.signUp);
 
 module.exports = router;
