8bbddbc5c0d70aa9788e2e2a6f412f6420262166
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2024-03-05 13:46:38 +0700

    cleanup codebase

--- nodejs/Lab 7/frontend/src/Order/orderLoader.js
+++
@@ -1,8 +0,0 @@
-export async function orderLoader() {
-  const res = await fetch("http://localhost:3000/get-orders");
-
-  if (res.ok) {
-    const resData = await res.json();
-    return resData;
-  }
-}
